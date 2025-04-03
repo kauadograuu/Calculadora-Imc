dec47522a5012a90c444be2c36bcfb1a86497409
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-04-03 12:14:10 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function imc(){
+    const nome = document.getElementById('nome').value
+    const altura = document.getElementById('altura').value
+    const peso = document.getElementById('peso').value
+}
