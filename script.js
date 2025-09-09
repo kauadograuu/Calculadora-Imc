cc502d64a355e73a5c74acc101d191c36d6c49ed
kauadograuu <melo.kaua@escola.pr.gov.br>
2025-09-09 12:05:58 -0300

    kauaa

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function imc(){
     const valorIMC = (peso/(altura*altura)).toFixed(1);
 
     resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
-}
 
+let classificacao = "";
+
+    if(valorIMC < 18.5){
+    classificacao = "abaixo do peso";
+    }else if(valorIMC < 25){
+        classificacao = "peso ideal!";
+    }else if(valorIMC < 30){
+        classificacao = "levemente acima do peso";
+    }else{
+        classificacao = "obesidade";
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC}!`; , sua classificacao é $'(classificacao)';
+}
 calcular.addEventListener('click',imc);
